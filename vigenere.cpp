3c10dcab069222c46d8c673fa76ad77b11f31fe6
Peter Rauhut <dabeor@gmail.com>
2015-03-13 18:06:35 -0500

    [FEATURE] Add option parsing and argument assignement.\n[IMPROVEMENT] Add formatting to printTable() and printHelp().

--- vigenere.cpp
+++ vigenere.cpp
@@ -2,56 +2,80 @@
 
 using std::cout; 
 using std::endl;
+using std::string;
+
+// Global configurations
+enum OPERATION {NONE, ENCRYPT, DECRYPT, CRACK, PRINT};
 
 // Print command line help
 void printHelp() {
-    cout << "usage: vigenere [-p][-h]" << endl;
-}
-
-char getOption(char arg[], int j){
-    return arg[j];
+    cout << "usage: vigenere [-h | -e key text| -d key cipher | -c cipher | -p | -h]" << endl;
+    cout << "\tOptions: " << endl;
+    cout << "\t\t-h\tPrint this help message" << endl << endl;
+    cout << "\t\t-e\tRequires 2 arguments. Uses the provided key to\n\t\t\tencrypt the text." << endl << endl;
+    cout << "\t\t-d\tRequires 2 arguments. Uses the provided key to\n\t\t\tdecrypt the cipher." << endl << endl;
+    cout << "\t\t-c\tRequires 1 argument. Attempts to decode the\n\t\t\tprovided cipher." << endl << endl;
+    cout << "\t\t-p\tPrints a table displaying the standard values\n\t\t\tof a vignere cipher." << endl << endl;
 }
 
 // Print out an all capital letter version of a vigenere table using standard English characters.
 void printTable() {
     char a = 'A', z='Z';
     int offset = 0;
-                
+    cout << endl;            
     for(char c = a; c <= z; c++) {
+        cout << "\t" <<c << " | ";
         for(char i = a + offset; i <= z + offset; i++) {
             cout << (char)(i <= z ? i : i - 26) << " "  ;
         }   
         offset++;
         cout << endl;
     }   
+    cout << endl;
 }
 
 // Parse command line option
-int parseOptions(int argc, char** argv){
-        for(int i = 1; i < argc; i++){
-            if (argv[i][0] == '-') {
-                int j = 1;
-                while(char c = getOption(argv[i], j)) {
-                    j++;
-                    switch (c) {
-                        case 'h': printHelp(); return -1;
-                        case 'e': cout << "Encrypt not yet added" << endl; return -1;
-                        case 'd': cout << "Decrypt not yet added" << endl; return -1;
-                        case 'c': cout << "Solve cipher not yet added" << endl; return -1;
-                        case 'p': printTable(); break;
-                        case 'k': cout << "Solve for key not yet added" << endl; return -1;
-                        default : printHelp(); break;
-                    }
-                }
-            } else {
-                cout << argv[i] << endl;
-            }
-        }
+OPERATION parseOptions(int argc, char** argv){
+    char c; 
+    if (argv[1][0] = '-') {
+        c = argv[1][1];
+    } else {
+        return NONE;
+    }
+    
+    switch (c) {
+        case 'h': return NONE;
+        case 'e': return argv[2] ? (argv[3] ? ENCRYPT : NONE) : NONE;
+        case 'd': return argv[2] ? (argv[3] ? DECRYPT : NONE) : NONE;
+        case 'c': return argv[2] ? CRACK : NONE;
+        case 'p': return PRINT;
+        default : return NONE;
+    }
 }
 
 int main(int argc, char** argv) {
+    OPERATION op = parseOptions(argc, argv);
+    char* key, * text, * cipher;
 
-    parseOptions(argc, argv);
+    switch (op) {
+        case NONE   : printHelp(); break;
+        case ENCRYPT: 
+            cout << "ENCRYPT" << endl << endl; 
+            key = argv[2];
+            text = argv[3];
+            break;
+        case DECRYPT: 
+            cout << "DECRYPT" << endl << endl; 
+            key = argv[2];
+            cipher = argv[3];
+            break;
+        case CRACK  : 
+            cout << "CRACK" << endl << endl; 
+            cipher = argv[2];
+            break;
+        case PRINT  : printTable(); break;
+        default     : printHelp(); break;
+    }
 
     return 0;
 }
